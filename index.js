e50ceb1cf4405a4352654c8d5794a82d68d0109e
Ben Edwards <ben.edwards@benumu.com>
2018-10-22 13:35:47 +0100

    some docs`

--- index.js
+++ index.js
@@ -1,5 +1,14 @@
 /** @format */
 
+/**
+sudo dd if=kali-linux-light-2018.3a-i386.iso of=/dev/sdb bs=512k
+ cd ${ANDROID_HOME}/emulator
+ ./emulator -avd  Nexus_5X_API_28_x86
+
+  sudo qemu-system-x86_64 -enable-kvm -m 512 -hda ~/Downloads/kali-linux-light-2018.3a-amd64.iso
+  * 
+ * */
+
 import {AppRegistry} from 'react-native';
 import App from './src/App';
 import {name as appName} from './app.json';
