cb472531c9cd24627774231e5aa16635e326e022
Ben Edwards <ben.edwards@benumu.com>
2018-10-23 06:06:34 +0100

    wipe-data to clear cache

--- index.js
+++ index.js
@@ -3,7 +3,7 @@
 /**
 sudo dd if=kali-linux-light-2018.3a-i386.iso of=/dev/sdb bs=512k
  cd ${ANDROID_HOME}/emulator
- ./emulator -avd  Nexus_5X_API_28_x86
+ ./emulator -avd  Nexus_5X_API_28_x86 -wipe-data
 
   sudo qemu-system-x86_64 -enable-kvm -m 512 -hda ~/Downloads/kali-linux-light-2018.3a-amd64.iso
   * 
